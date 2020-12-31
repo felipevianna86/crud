9d82efb4b7b1983652866d50e9e181c9afddcabc
felipevianna <felipe.vianna86@gmail.com>
2020-12-30 21:49:13 -0300

    #3 - Criando métodos para CRUD em ProdutoService.

--- src/main/java/com/spring/cloud/crud/service/ProdutoService.java
+++ src/main/java/com/spring/cloud/crud/service/ProdutoService.java
@@ -1,9 +1,15 @@
 package com.spring.cloud.crud.service;
 
 import com.spring.cloud.crud.data.vo.ProdutoVO;
+import com.spring.cloud.crud.entity.Produto;
+import com.spring.cloud.crud.exception.ResourceNotFoundException;
 import com.spring.cloud.crud.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -16,7 +22,46 @@ public class ProdutoService {
     }
 
     public ProdutoVO create(ProdutoVO produtoVO){
+        Produto produto = produtoRepository.save(converterToProduto(produtoVO));
+        return converterToProdutoVO(produto);
+    }
 
-        return null;
+    public Page<ProdutoVO> findAll(Pageable pageable){
+        var page = produtoRepository.findAll(pageable);
+        return page.map(this::converterToProdutoVO);
+    }
+
+    public ProdutoVO findById(Long id){
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        return converterToProdutoVO(entity);
+    }
+
+    public ProdutoVO update(ProdutoVO produtoVO){
+        final Optional<Produto> optionalProduto = produtoRepository.findById(produtoVO.getId());
+
+        if(!optionalProduto.isPresent()){
+            new ResourceNotFoundException("No records found for this ID");
+        }
+
+        Produto produto = produtoRepository.save(converterToProduto(produtoVO));
+
+        return converterToProdutoVO(produto);
+    }
+
+    public void delete(Long id){
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        produtoRepository.delete(entity);
+    }
+
+    private ProdutoVO converterToProdutoVO(Produto produto){
+        return ProdutoVO.convertToProdutoVO(produto);
+    }
+
+    private Produto converterToProduto(ProdutoVO produtoVO){
+        return Produto.convertToProduto(produtoVO);
     }
 }
