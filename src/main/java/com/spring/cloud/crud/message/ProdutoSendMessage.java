914bfad26d6f98bf6430d9f160c3214c313a6223
felipevianna <felipe.vianna86@gmail.com>
2021-01-16 22:57:14 -0300

    #6 - Adicionando RabbitMQ ao projeto e criando producer.

---
+++ src/main/java/com/spring/cloud/crud/message/ProdutoSendMessage.java
@@ -0,0 +1,28 @@
+package com.spring.cloud.crud.message;
+
+import com.spring.cloud.crud.data.vo.ProdutoVO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoSendMessage {
+
+    @Value("${crud.rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${crud.rabbitmq.routingkey}")
+    String routingkey;
+
+    public final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ProdutoSendMessage(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(ProdutoVO produtoVO){
+        rabbitTemplate.convertAndSend(exchange, routingkey, produtoVO);
+    }
+}
